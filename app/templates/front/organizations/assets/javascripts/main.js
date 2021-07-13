795ad06548e1b8f2a78387518904f9df04886660
Jean <jean@idocus.com>
2021-07-13 11:12:06 +0300

    Organization: general configuration tab

--- app/templates/front/organizations/assets/javascripts/main.js
+++ app/templates/front/organizations/assets/javascripts/main.js
@@ -7,18 +7,22 @@
 //= require jquery.livequery.min
 
 jQuery(function () {
-  $(".principal li > span.link_principal").unbind('click');
-  $(".principal li > span.link_principal").bind('click',function(e) {
-    e.stopPropagation()
-    $('.principal li span.link_principal').removeClass('active')
-    $(this).parent().find('span.link_principal').addClass('active')
+  $(".principal li > span.link_principal, .principal .slave li > span.link_slave").unbind('click');
+  $(".principal li > span.link_principal, .principal .slave li > span.link_slave").bind('click',function(e) {
+    e.stopPropagation();
+    $('.principal li span.link_principal').removeClass('active');
+    $(this).parent().find('span.link_principal').addClass('active');
     if ($(this).parent().data('href') == "parametres"){
-      $(this).parent().find('.chevron').toggle()
-      $(this).parent().find('ul').slideToggle()
+      $(this).parent().find('.chevron').toggle();
+      $(this).parent().find('ul').slideToggle();
+      $('.principal li span.link_slave').removeClass('active');
     }
     else if ($(this).parent().data('href') == "organization_home") {location.reload();}
     else{
       $(".organizations .content").html('');
+      if ($(this).hasClass('link_slave')){
+        $('.parameters').addClass('active')
+      }
       let url = '/organizations/'+$(this).parent().data('href')
       $.ajax({
         url: url, 
---
+++ app/templates/front/organizations/assets/javascripts/settings.js
@@ -0,0 +1,90 @@
+jQuery(function () {
+
+  /* Général */  
+  $('.valid-modification').unbind('click')
+  $(".valid-modification").bind('click',function(e) {
+      e.stopPropagation()
+      $('#general-modal').modal('show')
+  });
+  /* Général */
+
+  /* Nommage PDF */
+  $('#sortable').sortable({
+    items: "li.btn-light.active",
+    start: function(event, ui) {      
+      ui.item.unbind("click");
+    },
+    stop: function(event, ui) {      
+      ui.item.bind('click', function(){});  
+    }
+  });
+  $('#element-separator').multiSelect();
+
+
+  $('li.btn-light.click').unbind('click')
+  $('li.btn-light.click').on('click',function(e) {
+      e.preventDefault()
+      $(this).hasClass('active') ? $(this).removeClass('active') : $(this).addClass('active')
+  })
+  /* Nommage PDF */
+
+
+  /* Email rappel */
+  $('.action.sub_integration').unbind('click')
+  $(".action.sub_integration").bind('click',function(e) {
+    e.stopPropagation()
+
+    if ($(this).parent().find('div.sub_menu').length > 0){
+      $(this).parent().find('div.sub_menu').remove()
+    }
+    else {
+      if ($('.action .sub_menu').length > 0){
+        $('.action .sub_menu').remove()
+      }
+
+      $(this).append($('.sub_integration_append').html());
+    }
+  });
+
+  $('.new-mail-rappel').unbind('click')
+  $(".new-mail-rappel").bind('click',function(e) {
+      e.stopPropagation()
+      $('#mail-rappel-modal').modal('show')
+  });
+  /* Email rappel */
+
+  /* Logiciel Compta */
+  $('#piece-name-edit').unbind('click')
+  $("#piece-name-edit").bind('click',function(e) {
+    e.stopPropagation()
+    $('#softwares-piece-modal').modal('show')
+  });
+
+
+  $('#preseizure-name-edit').unbind('click')
+  $("#preseizure-name-edit").bind('click',function(e) {
+    e.stopPropagation()
+    $('#softwares-preseizure-modal').modal('show')
+  });
+
+  $('#sortable-software').sortable({
+    items: "li.btn-light",
+    start: function(event, ui) {      
+      ui.item.unbind("click");
+    },
+    stop: function(event, ui) {      
+      ui.item.bind('click', function(){});  
+    }
+  });
+
+  $('#sortable-software-preseizures').sortable({
+    items: "li.btn-light",
+    start: function(event, ui) {      
+      ui.item.unbind("click");
+    },
+    stop: function(event, ui) {      
+      ui.item.bind('click', function(){});  
+    }
+  });
+  /* Logiciel Compta */
+});
