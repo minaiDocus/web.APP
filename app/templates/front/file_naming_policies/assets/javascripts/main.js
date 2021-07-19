fa18e6dc74b16b3d2cb01a13dc0dff0775ad7b76
Jean <jean@idocus.com>
2021-07-19 10:20:02 +0300

    Organization: move file naming policies pdf new templates

--- app/templates/front/file_naming_policies/assets/javascripts/main.js
+++ app/templates/front/file_naming_policies/assets/javascripts/main.js
@@ -1,3 +1,20 @@
 jQuery(function() {
-  console.log('file storage authorizations todo ...');
+  $('#sortable').sortable({
+    items: "li.btn-light.active",
+    start: function(event, ui) {
+      ui.item.unbind("click");
+    },
+    stop: function(event, ui) {
+      ui.item.bind('click', function(){});
+    }
+  });
+
+  // $('#element-separator').multiSelect();
+
+
+  $('li.btn-light.click').unbind('click');
+  $('li.btn-light.click').on('click',function(e) {
+    e.preventDefault();
+    $(this).hasClass('active') ? $(this).removeClass('active') : $(this).addClass('active');
+  });
 });
