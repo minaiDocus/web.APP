488fc03d7fd8a5445651133c25e1f91e750c9219
Jean <jean@idocus.com>
2021-08-31 13:09:06 +0300

    Organization: paper set orders && fixes conditions views

--- app/templates/front/paper_set_orders/assets/javascripts/main.js
+++ app/templates/front/paper_set_orders/assets/javascripts/main.js
@@ -1,3 +1,40 @@
-jQuery(function() {
-  console.log('file storage authorizations todo ...');
+jQuery(function () {
+  $('.commande-kit').unbind('click')
+  $(".commande-kit").bind('click',function(e) {      
+    e.stopPropagation();
+
+    $('#select-kit').searchableOptionList({
+      'searchplaceholder': 'Selectionner / Rechercher un dossier client à qui envoyer un kit courrier'
+    });
+
+    $('#commande-courrier').modal('show')
+  });
+
+  $('.action.sub-menu-box, .action.sub-menu-kit').unbind('click')
+  $(".action.sub-menu-box, .action.sub-menu-kit").bind('click',function(e) {
+    e.stopPropagation();
+
+    if ($(this).find('.sub_menu').hasClass('hide')){
+      $(this).find('.sub_menu').removeClass('hide')
+    }
+    else {
+      $(this).find('.sub_menu').addClass('hide')
+    }
+  });
+
+  $('.sub_menu li.edit').unbind('click');
+  $('.sub_menu li.edit').bind('click', function(e){
+
+    $('#kit-state').multiSelect({
+      'noneText': 'Selectionner le statut'
+    });
+
+    $('#edit-kit-box').modal('show')
+  });
+
+  $('.sub_menu li.delete').unbind('click');
+  $('.sub_menu li.delete').bind('click', function(e){
+
+    $(this).closest('tr').remove();
+  });
 });
