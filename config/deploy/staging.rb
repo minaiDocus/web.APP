7079f515569e00584139ba1d7d675b20caf1ce3e
Mina RAKOTONIRAINY <mina@idocus.com>
2021-10-21 11:03:49 +0300

    Config deploiment for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,10 +2,10 @@ set :deploy_to, '/home/deploy/my'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('files')
 
-set :repo_url, 'git@github.com:i-docus/main.git'
+set :repo_url, 'git@github.com:i-docus/web.APP.git'
 
 set :branch, 'staging'
 
 set :rvm_ruby_version, '2.6.5'
 
-server 'my.staging.idocus.com', user: 'deploy', roles: %w{app db web}
+server 'staging-v3.idocus.com', user: 'deploy', roles: %w{app db web worker}
