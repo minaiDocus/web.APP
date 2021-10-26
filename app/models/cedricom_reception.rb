a580b360e22f09f1036fd11be774bb7a4686b8d6
Thomas ANDRIEU <t.andrieu@gmail.com>
2021-10-26 18:16:23 +0200

    fix cedricom

--- app/models/cedricom_reception.rb
+++ app/models/cedricom_reception.rb
@@ -1,8 +1,9 @@
 class CedricomReception < ApplicationRecord
   has_one_attached :content
 
+  belongs_to :organization
   has_many :operations
-  
+
   validates_uniqueness_of :cedricom_id, scope: :organization_id
 
   scope :to_download, -> { where(downloaded: false) }
