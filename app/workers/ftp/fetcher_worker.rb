ddb09e2c0e807d6b04a66c9b5f76b2fc3ebd311c
Mina RAKOTONIRAINY <mina@idocus.com>
2021-11-03 15:25:22 +0300

    TempFix: Check FTP fetcher folder

--- app/workers/ftp/fetcher_worker.rb
+++ app/workers/ftp/fetcher_worker.rb
@@ -5,12 +5,53 @@ class Ftp::FetcherWorker
   def perform
     if FTPDeliveryConfiguration::IS_ENABLED
       UniqueJobs.for 'FtpFetcher' do
-        Ftp::Fetcher.fetch(FTPDeliveryConfiguration::FTP_SERVER,
-                           FTPDeliveryConfiguration::FTP_USERNAME,
-                           FTPDeliveryConfiguration::FTP_PASSWORD,
-                           FTPDeliveryConfiguration::FTP_PATH,
-                           FTPDeliveryConfiguration::FTP_PROVIDER)
+        ### TEMP FIX : SEND FETCHER MANUALLY
+        # Ftp::Fetcher.fetch(FTPDeliveryConfiguration::FTP_SERVER, FTPDeliveryConfiguration::FTP_USERNAME, FTPDeliveryConfiguration::FTP_PASSWORD, FTPDeliveryConfiguration::FTP_PATH, FTPDeliveryConfiguration::FTP_PROVIDER)
+
+
+        Ftp::FetcherWorker::Launcher.check_folder( FTPDeliveryConfiguration::FTP_SERVER,
+                                         FTPDeliveryConfiguration::FTP_USERNAME,
+                                         FTPDeliveryConfiguration::FTP_PASSWORD,
+                                         FTPDeliveryConfiguration::FTP_PATH,
+                                         FTPDeliveryConfiguration::FTP_PROVIDER)
       end
     end
   end
+
+  class Launcher
+    def self.check_folder(url, username, password, dir = '/', provider = '')
+      ftp = Net::FTP.new
+      ftp.connect url, 21
+      ftp.login username, password
+      ftp.passive = true
+
+      ftp.chdir dir
+
+      dirs = ftp.nlst.sort
+
+      new_dir = dirs.select do |e|
+        !e.end_with?('ready') && !e.end_with?('fetched') && !e.end_with?('errors') && !e.end_with?('ACCOMPLYS')
+      end
+
+
+      if new_dir.any?
+        p "======= New documents found =========="
+        log_document = {
+          subject: "[FtpFetcher] - new documents injected",
+          name: "ftp fetcher - new docs",
+          error_group: "[FtpFetcher] new documents injected",
+          erreur_type: "[FtpFetcher] - new documents injected",
+          date_erreur: Time.now.strftime('%Y-%m-%d %H:%M:%S'),
+          more_information: {
+            dirs: new_dir.join(' / '),
+          }
+        }
+
+        ErrorScriptMailer.error_notification(log_document).deliver
+      else
+        p "============ No new documents=================="
+      end
+    end
+  end
+
 end
