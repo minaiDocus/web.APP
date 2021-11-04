0b81fb2148b1f7f2e4bc2a4f8faf3467c9f9f48c
Mina RAKOTONIRAINY <mina@idocus.com>
2021-11-04 09:26:59 +0300

    Minor fixe

--- app/mailers/fetch_operations_mailer.rb
+++ app/mailers/fetch_operations_mailer.rb
@@ -2,6 +2,6 @@ class FetchOperationsMailer < BaseMailer
   def notify(notify_content)
     @notify_content = notify_content
 
-    mail to: Settings.first.notify_errors_to, subject: "[Fetch Operation] - Détail du #{@notify_content[:date_fetch]}"[0..200] if @notify_content[:details].any?
+    mail to: Settings.first.notify_errors_to, subject: "[Fetch Operation] - Détail du #{@notify_content[:date_fetch]}"[0..200] if @notify_content[:details].present?
   end
 end
