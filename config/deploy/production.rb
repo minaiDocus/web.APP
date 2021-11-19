71386c9e1779e687d1d764e562e10a795f0c23f7
Mina RAKOTONIRAINY <mina@idocus.com>
2021-11-19 06:44:19 +0300

    Temp: conf staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,11 +2,8 @@ set :deploy_to, '/home/deploy/main/'
 
 set :repo_url, 'git@github.com:i-docus/web.APP.git'
 
-set :branch, 'master'
+set :branch, 'next_ss'
 
 set :rvm_ruby_version, '2.6.5'
 
-server 'app-1.idocus.tech', user: 'deploy', roles: %w{app db web}
-server 'app-2.idocus.tech', user: 'deploy', roles: %w{app web}
-server 'sidekiq-1.idocus.tech', user: 'deploy', roles: %w{app worker}
-server 'sidekiq-2.idocus.tech', user: 'deploy', roles: %w{app worker}
+server 'staging-v3.idocus.com', user: 'deploy', roles: %w{app db web worker}
