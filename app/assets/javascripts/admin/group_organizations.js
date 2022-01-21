e4328cdd3c1a282bfbcdeb49384864ce59bcb79c
Paul <paul@idocus.com>
2022-01-21 11:42:42 +0300

    Admin next view : Process reporting

--- app/assets/javascripts/admin/group_organizations.js
+++
@@ -1,7 +0,0 @@
-jQuery(function () {  
-  if ($('#group_organizations .searchable-option-list').length > 0) {   
-    $('#group_organizations .searchable-option-list').searchableOptionList({     
-      maxHeight: '300px'     
-    })
-  }
-});
