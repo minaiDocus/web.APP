e8bf036399d859e33bdb8b4fbd0eba8e101f900b
Mina RAKOTONIRAINY <mina@idocus.com>
2022-03-01 11:11:27 +0300

    Create auxilium customer via CSV - next csv

--- app/services/ponctual_scripts/create_customer_via_csv.rb
+++ app/services/ponctual_scripts/create_customer_via_csv.rb
@@ -1,6 +1,6 @@
 class PonctualScripts::CreateCustomerViaCsv
   def initialize(file_path=nil, requester_code=nil)
-    @file_path      = file_path || Rails.root.join('spec/support/files/ponctual_scripts/new_customer.csv')
+    @file_path      = file_path || Rails.root.join('spec/support/files/ponctual_scripts/new_customer_2.csv')
     @requester_code = requester_code || 'ALM%ZG'
   end
 
@@ -34,34 +34,36 @@ class PonctualScripts::CreateCustomerViaCsv
       end
 
       if organization && customer.persisted?
-        bank_name = ''
-        match_bank_name.each{ |_name| bank_name = _name if bank_name.blank? && data[13].downcase.include?(_name.tr('éèếÉ', 'eeee').downcase) }
+        if data[11].strip.present?
+          bank_name = ''
+          match_bank_name.each{ |_name| bank_name = _name if bank_name.blank? && data[13].downcase.include?(_name.tr('éèếÉ', 'eeee').downcase) }
 
-        if bank_name.blank?
-          data_not_created << { user: customer.code, bank_name: data[13] }
-        end
+          if bank_name.blank?
+            data_not_created << { user: customer.code, bank_name: data[13] }
+          end
 
-        bank_account                   = customer.bank_accounts.where(api_name: 'idocus', name: bank_name, number: data[11].strip).first || BankAccount.new
-        bank_account.user              = customer
-        bank_account.api_name          = 'idocus'
-        bank_account.bank_name         = bank_name
-        bank_account.name              = data[13].strip
-        bank_account.number            = data[11].strip
-        bank_account.journal           = data[15].strip
-        bank_account.currency          = data[12].strip
-        bank_account.original_currency = {"id"=>"EUR", "symbol"=>"€", "prefix"=>false, "precision"=>2, "marketcap"=>nil, "datetime"=>nil, "name"=>"Euro"}
-        bank_account.accounting_number = data[14].strip
-        bank_account.temporary_account = "471000"
-        bank_account.start_date        = data[16].gsub(/\n/, '')
-        bank_account.is_used           = true
+          bank_account                   = customer.bank_accounts.where(api_name: 'idocus', name: bank_name, number: data[11].strip).first || BankAccount.new
+          bank_account.user              = customer
+          bank_account.api_name          = 'idocus'
+          bank_account.bank_name         = bank_name
+          bank_account.name              = data[13].strip
+          bank_account.number            = data[11].strip
+          bank_account.journal           = data[15].strip
+          bank_account.currency          = data[12].strip
+          bank_account.original_currency = {"id"=>"EUR", "symbol"=>"€", "prefix"=>false, "precision"=>2, "marketcap"=>nil, "datetime"=>nil, "name"=>"Euro"}
+          bank_account.accounting_number = data[14].strip
+          bank_account.temporary_account = "471000"
+          bank_account.start_date        = data[16].gsub(/\n/, '')
+          bank_account.is_used           = true
 
-        data_bank << { csv: data[13], base: bank_name.titleize }  if !bank_name.blank? && bank_name.downcase != data[13].downcase
+          data_bank << { csv: data[13], base: bank_name.titleize }  if !bank_name.blank? && bank_name.downcase != data[13].downcase
 
-        if not bank_account.save
-          data_errors << { code: data[2], error_bank_message: bank_account.try(:errors).try(:messages) }
+          if not bank_account.save
+            data_errors << { code: data[1], error_bank_message: bank_account.try(:errors).try(:messages) }
+          end
         end
       else
-        data_errors << { code: data[2], message: customer.try(:errors).try(:messages) }
+        data_errors << { code: data[1], message: customer.try(:errors).try(:messages) }
       end
     end
 
