1374396e67b8a37263af0637a49c003c948127c2
Mina RAKOTONIRAINY <mina@idocus.com>
2022-03-14 17:23:32 +0300

    New facturation

---
+++ app/modules/package/pricing.rb
@@ -0,0 +1,144 @@
+class Package::Pricing
+  LISTS = {
+            ido_classic: {
+              human_name: "iDo'Classique",
+              description: "Vous permet de transférer jusqu'à 100 pièces/mois, mutualisation des quotas au niveau du cabinet.",
+              hint: "Au-delà du quota cabinet cumulé, calcul du dépassement simplifié : 0,25€ ht/facture",
+              price: 20,
+              commitment: 0,
+              data_flows: { max: 100, duration: 'month', excess_price: 0.25 },
+              options: { upload: 'strict', bank: 'optional', scan: 'strict', preassignment: 'optional', mail: 'optional'}
+            },
+            ido_nano: {
+              human_name: "iDo'Nano",
+              description: "vous permet de transférer jusqu'à 100 pièces/mois, mutualisation des quotas au niveau du cabinet.",
+              hint: "Au-delà du quota cabinet cumulé, calcul du dépassement simplifié : 0,25€ ht/facture",
+              price: 5,
+              commitment: 12,
+              data_flows: { max: 100, duration: 'annual', excess_price: 0.25 },
+              options: { upload: 'strict', bank: 'none', scan: 'strict', preassignment: 'strict', mail: 'optional'}
+            },
+            ido_micro: {
+              human_name: "iDo'Micro",
+              description: "vous permet de transférer jusqu'à 100 pièces/mois, mutualisation des quotas au niveau du cabinet.",
+              hint: "Au-delà du quota cabinet cumulé, calcul du dépassement simplifié : 0,25€ ht/facture",
+              price: 10,
+              commitment: 12,
+              data_flows: { max: 100, duration: 'annual', excess_price: 0.25 },
+              options: { upload: 'strict', bank: 'strict', scan: 'strict', preassignment: 'strict', mail: 'optional'}
+            },
+            ido_micro_plus: {
+              human_name: "iDo'Micro",
+              description: "vous permet de transférer jusqu'à 100 pièces/mois, mutualisation des quotas au niveau du cabinet.",
+              hint: "Au-delà du quota cabinet cumulé, calcul du dépassement simplifié : 0,25€ ht/facture",
+              price: 10,
+              commitment: 12,
+              data_flows: { max: 25, duration: 'month', excess_price: 0.3 },
+              options: { upload: 'strict', bank: 'optional', scan: 'strict', preassignment: 'strict', mail: 'optional'}
+            },
+            ido_x: {
+              human_name: "iDo'X",
+              description: "vous permet de transférer jusqu'à 100 pièces/mois, mutualisation des quotas au niveau du cabinet.",
+              hint: "Au-delà du quota cabinet cumulé, calcul du dépassement simplifié : 0,25€ ht/facture",
+              price: 5,
+              commitment: 0,
+              data_flows: { max: 0, duration: 'month', excess_price: 0.25 },
+              options: { upload: 'none', bank: 'none', scan: 'none', preassignment: 'strict', mail: 'none'}
+            },
+            ido_retriever: {
+              human_name: "Automate",
+              description: "Vous permet de bénéficier des automates de récupération bancaires",
+              hint: "",
+              price: 5,
+              commitment: 0,
+              data_flows: { max: 0, duration: 'month', excess_price: 0.25 },
+              options: { upload: 'none', bank: 'strict', scan: 'none', preassignment: 'strict', mail: 'none'}
+            },
+            ido_digitize: {
+              human_name: "Numérisation",
+              description: "vous permet de transférer jusqu'à 100 pièces/mois, mutualisation des quotas au niveau du cabinet.",
+              hint: "",
+              price: 0,
+              commitment: 0,
+              data_flows: { max: 0, duration: 'month', excess_price: 0.25 },
+              options: { upload: 'none', bank: 'none', scan: 'strict', preassignment: 'strict', mail: 'none'}
+            },
+          }.freeze
+
+  class << self
+    def human_name_of(package)
+      Package::Pricing::LISTS[package.to_sym][:human_name]
+    end
+
+    def options_of(package)
+      Package::Pricing::LISTS[package.to_sym][:options]
+    end
+
+    def price_of(package)
+      return 9   if package.to_s == 'preassignment' #pre_assignment option coast 9€
+      return 10  if package.to_s == 'mail' #mail option coast 10€
+      return 2   if package.to_s == 'bank_excess' #Each excessed bank coast 5€
+      return 1   if package.to_s == 'journal_excess' #Each excessed journal coast 1€
+
+      Package::Pricing::LISTS[package.to_sym][:price]
+    end
+
+    def flow_limit_of(package)
+      Package::Pricing::LISTS[package.to_sym][:data_flows][:max]
+    end
+
+    def excess_price_of(package)
+      Package::Pricing::LISTS[package.to_sym][:data_flows][:excess_price]
+    end
+
+    def excess_duration_of(package)
+      Package::Pricing::LISTS[package.to_sym][:data_flows][:duration]
+    end
+
+    def commitment_duration_of(package)
+      Package::Pricing::LISTS[package.to_sym][:commitment]
+    end
+
+    def discount_prices(package, count, version=1)
+      prices = {
+                  '1' =>  [
+                              { limit: (0..75), package_price: 0, retriever_price: 0 },
+                              { limit: (76..150), package_price: -1, retriever_price: 0 },
+                              { limit: (151..250), package_price: -1.5, retriever_price: -0.5 },
+                              { limit: (251..350), package_price: -2, retriever_price: -0.75 },
+                              { limit: (351..500), package_price: -3, retriever_price: -1 },
+                              { limit: (501..Float::INFINITY), package_price: -4, retriever_price: -1.25 }
+                          ],
+                  '2' =>  [
+                            { limit: (0..250), package_price: 0, retriever_price: 0 },
+                            { limit: (251..Float::INFINITY), package_price: -10, retriever_price: 0 }
+                          ],
+                  '3' =>  [
+                            { limit: (0..50), package_price: -1, retriever_price: 0 },
+                            { limit: (51..150), package_price: -1.5, retriever_price: -0.5 },
+                            { limit: (151..200), package_price: -2, retriever_price: -0.75 },
+                            { limit: (201..250), package_price: -2.5, retriever_price: -1 },
+                            { limit: (251..350), package_price: -3, retriever_price: -1.25 },
+                            { limit: (351..500), package_price: -4, retriever_price: -1.50 },
+                            { limit: (501..Float::INFINITY), package_price: -5, retriever_price: -2 }
+                          ],
+                         
+               }
+
+      _price = 0
+      _found = false
+
+      prices[version.to_s].each do |node|
+        next if found
+
+        if ['ido_retriever', 'bank_option'].include?(package)
+          _price = node[:retriever_price]  if node[:limit].include?(count)
+        else
+          _price = node[:package_price] if node[:limit].include?(count)
+        end
+
+        _found = true if _price < 0
+      end
+    end
+  end
+end
---
+++ app/services/data_flow_service/calculator.rb
@@ -0,0 +1,59 @@
+class DataFlowService::Calculator
+  def self.execute(customers=nil)
+    new.execute(customers)
+  end
+
+  def initialize(date=Time.now)
+    @time = date
+    @period = CustomUtils.period_of(@time)
+  end
+
+  def execute(customers=nil)
+    _customers = Array(customers).presence || User.where(is_prescriber: false).active_at(@time)
+
+    _customers.each do |customer|
+      next if customer.is_prescriber || !customer.still_active?
+
+      preseizures       = customer.preseizures.where("DATE_FORMAT(created_at, '%Y%m') = #{@period}").where('piece_id > 0')
+      preseizures_piece = preseizures.where('piece_id > 0').count
+      preseizures_ope   = preseizures.where('operation_id > 0').count
+
+      operations_count  = customer.operations.where("DATE_FORMAT(created_at, '%Y%m') = #{@period}").count
+      pieces_count      = customer.pack_pieces.where("DATE_FORMAT(created_at, '%Y%m') = #{@period}").count
+      expences_count    = customer.expenses.where("DATE_FORMAT(created_at, '%Y%m') = #{@period}").count
+
+      data_flow                   = customer.get_flow_of(@period)
+      data_flow.pieces            = pieces_count
+      data_flow.operations        = operations_count
+      data_flow.compta_pieces     = preseizures_piece + expences_count
+      data_flow.compta_operations = preseizures_ope
+
+      data_flow.bank_excess    = bank_excess_of(customer)
+      data_flow.journal_excess = journal_excess_of(customer)
+
+      data_flow.scanned_sheets = scanned_sheet_of(customer)
+
+      data_flow.save
+    end
+  end
+
+  private
+
+  def bank_excess(customer)
+    bank_ids             = customer.operations.where("DATE_FORMAT(created_at, '%Y%m') = #{@period}").pluck(:bank_account_id).uniq
+    excess_bank_accounts = customer.bank_accounts.where("DATE_FORMAT(created_at, '%Y%m') <= #{@period}").where(id: bank_ids).size - 2
+
+    return (excess_bank_accounts > 0)? excess_bank_accounts : 0
+  end
+
+  def journal_excess(customer)
+    excess_journals_count = customer.account_book_types.count - 5
+
+    return (excess_journals_count > 0)? excess_journals_count : 0
+  end
+
+  def scanned_sheet_of(customer)
+    customer.temp_documents.where(api_name: 'scan').where("DATE_FORMAT(created_at, '%Y%m') = #{@period}").count
+  end
+
+end
