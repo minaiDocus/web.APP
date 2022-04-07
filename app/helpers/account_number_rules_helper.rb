65fc40d7ca9a6c03e7a6a3c7a0136f62b33b5e63
Mina RAKOTONIRAINY <mina@idocus.com>
2022-04-07 10:39:07 +0300

    Add *.* content number rules -- review

--- app/helpers/account_number_rules_helper.rb
+++ app/helpers/account_number_rules_helper.rb
@@ -13,6 +13,8 @@ module AccountNumberRulesHelper
     contents += content_tag :span, '- recherche: *station*2015 ne match pas avec libellé: Prélèvement préstation janvier 2015001', class: 'semibold'
     contents += content_tag :br, ''
     contents += content_tag :span, '- recherche: *station*2015* match avec libellé: Prélèvement préstation janvier 2015001', class: 'semibold'
+    contents += content_tag :br, ''
+    contents += content_tag :span, '- recherche: *.* match avec toutes les caractères', class: 'semibold'
 
     content_tag :div, contents.html_safe, class: 'w-100'
   end
