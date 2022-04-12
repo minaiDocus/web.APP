c2a5ef9069f270b717b4406ef11e265f604430e2
Mina RAKOTONIRAINY <mina@idocus.com>
2022-04-12 10:15:01 +0300

    Handle Cedricom list fetch error to avoid blocking process

--- app/workers/cedricom/get_list_worker.rb
+++ app/workers/cedricom/get_list_worker.rb
@@ -5,7 +5,23 @@ class Cedricom::GetListWorker
   def perform
     UniqueJobs.for 'GetList' do
       Organization.cedricom_configured.each do |organization|
-        Cedricom::FetchReceptions.new(organization).get_list
+        begin 
+          Cedricom::FetchReceptions.new(organization).get_list
+        rescue => e
+          log_document = {
+            subject: "[Cedricom-GetList] Error connexion",
+            name: "Cedricom-GetList",
+            error_group: "[cedricom] Error connexion",
+            erreur_type: "Error connexion",
+            date_erreur: Time.now.strftime('%Y-%M-%d %H:%M:%S'),
+            more_information: {
+              error_message: e.to_s,
+              organization: organization.try(:code)
+            }
+          }
+
+          ErrorScriptMailer.error_notification(log_document).deliver
+        end
       end
     end
   end
