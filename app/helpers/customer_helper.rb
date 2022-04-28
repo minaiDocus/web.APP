5b2258d038ef60419be32aa5e46f5cd415981d29
Paul <paul@idocus.com>
2022-04-28 13:56:14 +0300

    New facturation

--- app/helpers/customer_helper.rb
+++ app/helpers/customer_helper.rb
@@ -7,28 +7,42 @@ module CustomerHelper
 
     count_price = 0
 
-    debugger
+    if package
+      %w(name preassignment mail bank scan).each do |column|
+        next if BillingMod::Configuration.options_of(package.name)[column.to_sym] == 'strict'
 
-    if package            
-      content = ''
-      content_span = ''
-      count_price += BillingMod::Configuration.price_of(package)
+        _column = column != 'name' ? column.to_s +  '_active' : column
 
-      content_span += content_tag :span, option.group_title, class: 'semibold float-start'
-      content_span += content_tag :span, (option.price_in_cents_wo_vat * 0.01).to_s + '€', class: 'semibold float-end'
-      content += content_tag :div, content_span.html_safe, class: 'clearfix'
+        if package.send("#{_column}".to_sym)
+          column = 'ido_retriever' if column == 'bank'
+          column = 'ido_digitize'  if column == 'scan'
+          column = package.name    if column == 'name'
 
-      if option.title.present?
-        content += content_tag :i, option.title, class: 'clearfix'                 
-      end
+          content      = ''
+          content_span = ''
 
-      contents += content_tag :div, content.html_safe, class: 'p-2'
-      contents += content_tag :hr, '', class: 'm-1' 
+          price = BillingMod::Configuration.price_of(column.to_sym).to_i     
+          price -= BillingMod::Configuration.price_of(:preassignment).to_i if column == 'ido_classic' && package.name == 'ido_classic' && !package.preassignment_active
 
-      if index == period_option.size - 1
-        contents += content_tag :span, count_price.to_s + '€', class: 'semibold float-end p-1'
+          content_span  += content_tag :span, BillingMod::Configuration.human_name_of(column), class: 'semibold float-start'
+          content_span  += content_tag :span, price.to_s + '€', class: 'semibold float-end'
+          content       += content_tag :div, content_span.html_safe, class: 'clearfix'
+
+          if BillingMod::Configuration.label_of(column).present?
+            content += content_tag :i, BillingMod::Configuration.label_of(column), class: 'clearfix'
+          end
+          content += content_tag :hr, '', class: 'm-1'
+
+          contents += content_tag :div, content.html_safe, class: 'clearfix'
+
+          count_price += price.to_i
+        end
       end
     end
-    content_tag :div, contents.html_safe, class: 'w-100'   
+
+    contents += content_tag :hr, '', class: 'm-1'
+    contents += content_tag :span, count_price.to_s + '€', class: 'semibold float-end p-1'
+
+    content_tag :div, contents.html_safe, class: 'w-100'  
   end
 end
