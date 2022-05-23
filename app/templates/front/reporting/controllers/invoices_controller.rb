31e4bf6d828b2699737f6aad6ef1fa3861132a41
Mirana RAKOTONIRINA <mirana@idocus.com>
2022-05-23 09:01:16 +0300

    update invoice controller and invoice javascript

--- app/templates/front/reporting/controllers/invoices_controller.rb
+++ app/templates/front/reporting/controllers/invoices_controller.rb
@@ -8,9 +8,9 @@ class Reporting::InvoicesController < Reporting::ABaseController
 
   prepend_view_path('app/templates/front/reporting/views')
 
-  def period
-    render json: PeriodPresenter.new(@period, current_user).render_json, status: 200
-  end
+  # def period
+  #   render json: PeriodPresenter.new(@period, current_user).render_json, status: 200
+  # end
 
   def index0
     @year = begin
@@ -25,6 +25,8 @@ class Reporting::InvoicesController < Reporting::ABaseController
                     .order(start_date: :asc)
     @periods_by_users = periods.group_by { |period| period.user.id }.each do |_user, periods|
       periods.sort_by!(&:start_date)
+      puts "test period =  #{periods.first.current_packages}"
+      puts "user : #{@current_user}"
     end
 
     respond_to do |format|
@@ -39,14 +41,38 @@ class Reporting::InvoicesController < Reporting::ABaseController
   end
 
   def index
-    #User, BillingMod::Billing => app\mods\billing\models\billing.rb, BillingMod::DataFlow => data_flow.rb, BillingMod::Package => package.rb
+    @year = begin
+              Integer(params[:year])
+            rescue StandardError
+              Time.now.year
+            end
+
     render partial: 'index'
     puts "current user = #{current_user.name}"
     puts "current user's first name = #{current_user.first_name}"
     puts "current user's last name = #{current_user.last_name}"
     puts "current user's id = #{current_user.id}"
+    puts "current user's email = #{current_user.email}"
 
   end
+
+
+  def compute_total_billing_per_period(period, user_id)
+    return BillingMod::Billing.where(period: period, owner_id: user_id).sum(:price)
+  end
+  helper_method :compute_total_billing_per_period
+
+
+  def all_billings_per_period(period, user_id)
+    @all_billings = BillingMod::Billing.where(period: period, owner_id: user_id)
+    @total_billings = @all_billings.map(&:price).inject(0, :+)
+    puts "TOTAL Billing #{period} = #{@total_billings}"
+    
+    return @all_billings
+  end
+  helper_method :all_billings_per_period
+
+
 
   private
 
@@ -60,6 +86,7 @@ class Reporting::InvoicesController < Reporting::ABaseController
       render json: { json_flash: json_flash }, status: 200
     end
   end
+
 
 
 end
