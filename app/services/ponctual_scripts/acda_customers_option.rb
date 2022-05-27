f1c4a8860c36e219fa91266c02bfc2a52eb26a6e
Paul <bouhpaul@gmail.com>
2022-05-27 12:44:07 +0300

    Ponctual Script : deactivate mail option ACDA

---
+++ app/services/ponctual_scripts/acda_customers_option.rb
@@ -0,0 +1,36 @@
+class PonctualScripts::AcdaCustomersOption < PonctualScripts::PonctualScript
+  def self.execute
+    new().run
+  end
+
+  def self.rollback
+    new().rollback
+  end
+
+  private
+
+  def execute
+    current_period = CustomUtils.period_of(Time.now)
+    next_period    = CustomUtils.period_of(1.month.after)
+
+    organization = Organization.find_by_code 'ACDA'
+    user_code = []
+
+    organization.customers.active.each do |customer|
+      next if customer.orders.any?
+
+      current_package = customer.package_of(current_period)
+      next_package    = customer.package_of(next_period)
+
+      user_code << customer.code if current_package.try(:mail_active) || next_package.try(:mail_active)
+
+      current_package.update(mail_active: false) if current_package.try(:mail_active)
+      next_package.update(mail_active: false)    if next_package.try(:mail_active)
+    end
+
+    p user_code
+  end
+
+  def backup
+  end
+end
