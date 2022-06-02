bdbe2bf4f6d3a91989f69f7e41777b1db02793bd
Mina RAKOTONIRAINY <mina@idocus.com>
2022-06-02 23:45:00 +0300

    Fix invoice monthly report

--- app/services/organization/monthly_report.rb
+++ app/services/organization/monthly_report.rb
@@ -3,14 +3,15 @@
 class Organization::MonthlyReport
   def initialize(organization_id, customer_ids, time)
     @time = time
+    @period = CustomUtils.period_of(@time)
     @customer_ids    = customer_ids
     @organization_id = organization_id
   end
 
 
   def execute
-    if price_in_cents_wo_vat > 0
-      [formatted_price, periods.reject(&:organization).size]
+    if price_in_cents_w_vat > 0
+      [formatted_price, active_customers_size]
     else
       [nil, nil]
     end
@@ -18,17 +19,18 @@ class Organization::MonthlyReport
 
   private
 
-  def periods
-    @periods ||= Period.where('user_id IN (?) OR organization_id = ?', @customer_ids, @organization_id).where("start_date <= ? AND end_date >= ?", @time.to_date, @time.to_date)
+  def active_customers_size
+    date_end = @time.to_date.end_of_month
+    User.where(id: @customer_ids).active_at(date_end).count
   end
 
 
-  def price_in_cents_wo_vat
-    @price_in_cents_wo_vat ||= Billing::PeriodBilling.amount_in_cents_wo_vat(@time.month, periods)
+  def price_in_cents_w_vat
+    BillingMod::Invoice.where(organization_id: @organization_id).of_period(@period).first.try(:amount_in_cents_w_vat).to_f
   end
 
 
   def formatted_price
-    ('%0.2f' % (price_in_cents_wo_vat.round / 100.0)).tr('.', ',')
+    ('%0.2f' % (price_in_cents_w_vat.round / 100.0)).tr('.', ',')
   end
 end
