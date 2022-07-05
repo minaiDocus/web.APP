13c99626f759818c698bab6bb032e4ae6fc43083
Paul <paul@idocus.com>
2022-07-05 16:56:15 +0300

    Reporting Stat : Export XLS

--- app/templates/front/reporting/routes.rb
+++ app/templates/front/reporting/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     post '/injected_documents', to: 'statistics#injected_documents', as: 'report_injected_documents'
     post '/pre_assignment_accounts', to: 'statistics#pre_assignment_accounts', as: 'report_pre_assignment_accounts'
     post '/retrievers_report', to: 'statistics#retrievers_report', as: 'retrievers_report'
+
+    get '/export_xls/(:id)', to: 'statistics#export_xls', as: 'export_xls'
   end
 
   scope module: 'reporting' do
