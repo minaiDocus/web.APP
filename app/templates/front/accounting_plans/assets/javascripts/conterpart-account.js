76ed525a4b3911e00208ef1b1ae749657d7d49cf
Mina RAKOTONIRAINY <mina@idocus.com>
2022-07-26 16:09:43 +0300

    Renew conterpart accounts view

--- app/templates/front/accounting_plans/assets/javascripts/conterpart-account.js
+++ app/templates/front/accounting_plans/assets/javascripts/conterpart-account.js
@@ -2,16 +2,14 @@ class ConterpartAccount {
   constructor(){
     this.appJS            = new ApplicationJS;
     this.modal            = $('.modal#conterpart_accounts');
-    this.account_selector = $('select.account-item-selector');
     this.type             = 'provider';
   }
 
-  init_form(id=0){
+  init_form(type){
     let self = this;
-    let url  = $('input.conterpart-edition').val();
+    this.type = type;
+    let url   = $('input.conterpart-show').val();
     let param = `?kind=${self.type}`;
-    if(id > 0)
-      param = param + `&id=${id}`;
 
     let aj_params =   {
                         url: url + param,
@@ -19,23 +17,25 @@ class ConterpartAccount {
                         dataType: 'html',
                       }
     self.appJS.sendRequest(aj_params).then((res)=>{
-      this.modal.find('.modal-body .conterpart-body-form').html(res);
+      self.modal.find('.modal-body .conterpart-body-form').html(res);
+
+      self.account_selector = $('select.account-item-selector');
+      self.get_accounts_list();
     })
   }
 
-  delete_account(id=0){
-    if(!/^all-/.test(id) && id <= 0){ return false }
+  hide_additionnal_view(){
+    $('#conterpart-accounts-body .additionnal-fields').addClass('hide');
+    $('#conterpart-accounts-body .select-from-customer').addClass('hide');
+  }
 
-    let confirm_text   = 'Vous êtes sur le point de supprimer : ';
-    let account_to_del = $('.account-item-selector-container .account-item-selector option:selected').text();
-    if( /^all-/.test(id) ){
-      account_to_del = 'Tous les comptes de charge'
-    }
+  delete_account(ids = []){
+    if(ids.length < 0){ return false }
 
-    if(confirm(confirm_text + account_to_del + '\nEtes vous sûr?')){
+    if(confirm('Etes vous sûr?')){
       let self  = this;
       let url   = $('input.conterpart-deletion').val();
-      let param = `?id=${id}`;
+      let param = `?ids=${ids.join(',')}`;
 
       let aj_params =   {
                           url: url + param,
@@ -43,19 +43,17 @@ class ConterpartAccount {
                           dataType: 'json',
                         }
       self.appJS.sendRequest(aj_params).then((res)=>{
-        AppEmit('ca.new-conterpart-account');
         AppEmit('ca.refresh_accounts_list');
       })
     }
   }
 
-
-  get_accounts_list(type, reinit_form=true){
+  get_accounts_list(){
     let self  = this;
-    this.type = type;
+    this.hide_additionnal_view();
 
     let url  = $('input.conterpart-accounts-list').val();
-    url      = url.replace('/type', `/${type}`);
+    url      = url.replace('/type', `/${this.type}`);
 
     let aj_params =   {
                         url: url,
@@ -66,52 +64,119 @@ class ConterpartAccount {
       let selector_container = $('.account-item-selector-container');
       let selector = this.account_selector.clone();
       selector.html('');
-      selector.append('<option value="">Selectionner une catégorie</option>');
 
       res.accounts.forEach((account)=>{
         selector.append(`<option value='${account.id}'>${account.name} - ${account.number}</option>`);
       });
 
-      selector.addClass('chosen-list');
-      selector.unbind('change.account-selection').bind('change.account-selection', function(e){ self.init_form($(this).val()) });
+      // selector.addClass('chosen-list');
+      // selector.unbind('change.account-selection').bind('change.account-selection', function(e){ self.init_form($(this).val()) });
 
       selector_container.html(selector);
-
-      if(reinit_form){ self.init_form(0); }
     });
+  }
+
+  fetch_account(id, kind){
+    let self = this;
+    let url   = $('input.conterpart-edition').val();
+    let param = `?kind=${this.type}`;
+    if(id > 0)
+      param = param + `&id=${id}`;
+
+    this.hide_additionnal_view();
+    let aj_params =   {
+                        url: url + param,
+                        type: 'GET',
+                        dataType: 'html',
+                      }
+    self.appJS.sendRequest(aj_params).then((res)=>{
+      self.modal.find('.modal-body .additionnal-fields').html(res);
+      $('#conterpart-accounts-body .additionnal-fields').removeClass('hide');
+    })
+  }
+
+  add_from_customer(customer_id = 0){
+    if(customer_id > 0)
+    {
+      let self = this;
+      let url   = $('input.conterpart-add-from-customer').val();
+      let param = `?kind=${self.type}`;
+      param = param + `&selected_id=${customer_id}`
+
+      let aj_params =   {
+                          url: url + param,
+                          type: 'GET',
+                          dataType: 'json',
+                        }
+      self.appJS.sendRequest(aj_params).then((res)=>{
+        let selector_container = $('.select-conterpart-customer-container');
+        let selector = selector_container.find('.select-conterpart-customer').clone();
+        selector.html('');
+
+        if(res.accounts.length > 0)
+            $('.select-from-customer .select-from-customer-button').removeClass('hide');
+
+        res.accounts.forEach((account)=>{
+          selector.append(`<option value='${account.id}'>${account.name} - ${account.number}</option>`);
+        });
+
+        // selector.addClass('chosen-list');
+        // selector.unbind('change.account-selection').bind('change.account-selection', function(e){ self.init_form($(this).val()) });
+
+        selector_container.html(selector);
+      });
+    }
   }
 }
 
 jQuery(function() {
   let conterpart_account = new ConterpartAccount();
 
-  AppListenTo('ca.new-conterpart-account', (e)=>{
-    $('select.account-item-selector').val('');
-    conterpart_account.init_form(0);
+  AppListenTo('ca.edit-conterpart-account', (e)=>{
+    conterpart_account.fetch_account(0, 'conterpart_account');
+  });
+
+  AppListenTo('ca.show-customers-selection', (e)=>{
+    conterpart_account.hide_additionnal_view();
+    $('.select-from-customer .select-from-customer-button').addClass('hide');
+    $('#conterpart-accounts-body .select-from-customer').removeClass('hide');
+  });
+
+  AppListenTo('ca.add-from-customer', (e)=>{
+    let customer_id = e.detail.obj.val();
+    conterpart_account.add_from_customer(customer_id);
   });
 
   AppListenTo('ca.refresh_accounts_list', (e)=>{
     let type = $('.modal#conterpart_accounts').find('input#conterpart-kind').val();
-    conterpart_account.get_accounts_list(type, false);
+    conterpart_account.get_accounts_list();
   });
 
   AppListenTo('ca.delete-conterpart-account', (e)=>{
-    let type = e.detail.datas.type;
-    let kind = $('.modal#conterpart_accounts').find('input#conterpart-kind').val();
-
-    if(type == 'all')
-      conterpart_account.delete_account(`all-${kind}`);
-    else
-      conterpart_account.delete_account($('select.account-item-selector').val());
+    // let kind = $('.modal#conterpart_accounts').find('input#conterpart-kind').val();
+    conterpart_account.delete_account($('select.account-item-selector').val());
   });
 
+  AppListenTo('ca.select-conterpart', (e)=>{
+    let elem  = e.detail.obj;
+    let value = elem.val();
+
+    conterpart_account.fetch_account(parseInt(value[0]), 'conterpart_account');
+  });
+
+  // AppListenTo('ca.select-third-part', (e)=>{
+  //   let elem  = e.detail.obj;
+  //   let value = elem.val();
+
+  //   conterpart_account.get_account_infos(value[0], 'third_part');
+  // });
 
   AppListenTo('window.application_auto_rebind', (e)=>{
     $('button.manage-conterpart-accounts').unbind('click.manage-conterpart-accounts').bind('click.manage-conterpart-accounts', function(e){
       let type = ($(this).hasClass('customer'))? 'customer' : 'provider';
       $('.modal#conterpart_accounts').modal('show');
       $('.modal#conterpart_accounts').find('input#conterpart-kind').val(type);
-      conterpart_account.get_accounts_list(type);
+      conterpart_account.init_form(type);
     });
   });
 });
--- app/templates/front/accounting_plans/routes.rb
+++ app/templates/front/accounting_plans/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
           member do
             get 'accounts_list/:type', to: 'conterpart_account#accounts_list', as: 'accounts_list'
             delete 'delete', to: 'conterpart_account#delete', as: 'delete'
+            post 'link', to: 'conterpart_account#link', as: 'link'
+            get 'select_from_customer', to: 'conterpart_account#select_from_customer', as: 'select_from_customer'
+            post 'validate_from_customer', to: 'conterpart_account#validate_from_customer', as: 'validate_from_customer'
           end
         end
       end
