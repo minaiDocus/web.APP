06bbf9a36ca84ab6c329001bcbd66cc67a37a16d
Mina RAKOTONIRAINY <mina@idocus.com>
2022-11-17 21:33:16 +0300

    Optimize ponctual script

--- app/services/ponctual_scripts/fill_delivery_and_export_state.rb
+++ app/services/ponctual_scripts/fill_delivery_and_export_state.rb
@@ -1,32 +1,32 @@
 class PonctualScripts::FillDeliveryAndExportState < PonctualScripts::PonctualScript
-  def self.execute(piece_name)
-    new().run
+  def self.execute(date='2022-03-01')
+    new({date: date}).run
   end
 
   private
 
   def execute
-    preseizures = Pack::Report::Preseizure.where('created_at > ?', Date.parse('2021-08-01 23:59:59') )
+    preseizures = Pack::Report::Preseizure.where('created_at > ?', Date.parse("#{@options[:date]} 00:00:00"))
     total       = preseizures.size
 
     p total
 
     preseizures.each_with_index do |preseizure, index|
-      sleep(5) if (index % 150) == 0
+      sleep(5) if (index % 1000) == 0
 
       user = preseizure.user
 
       
       preseizure.delivery_state = 'not_delivered' if user.uses_api_softwares?
-      if preseizure.is_delivered_to.present? || preseizure.delivery_message.to_s.match(/already sent/) || preseizure.pre_assigment_deliveries.where(state: 'sent').size > 0
+      if preseizure.is_delivered_to.present? || preseizure.delivery_message.to_s.match(/already sent/) || preseizure.pre_assignment_deliveries.where(state: 'sent').size > 0
         preseizure.delivery_state = preseizure.is_delivered_to.presence || 'ibiza'
-      elsif preseizure.pre_assigment_deliveries.last.present?
+      elsif preseizure.pre_assignment_deliveries.last.present?
         preseizure.delivery_state = 'failed'
       end
 
       
       preseizure.export_state = 'not_exported' if user.uses_non_api_softwares?
-      export                  = preseizure.pre_assigment_exports.where(state: 'generated').last
+      export                  = preseizure.pre_assignment_exports.where(state: 'generated').last
       preseizure.export_state = export.for if export
 
       p "===#{index} / #{total}===="
