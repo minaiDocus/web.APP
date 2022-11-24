94431a2b00474c02dcfb0d488cea7baa7ff0ee68
Mina RAKOTONIRAINY <mina@idocus.com>
2022-11-24 10:12:58 +0300

    Add - Not Finalized Piece - to data verificator

---
+++ app/services/data_verificator/not_finalized_piece.rb
@@ -0,0 +1,20 @@
+# -*- encoding : UTF-8 -*-
+class DataVerificator::NotFinalizedPiece < DataVerificator::DataVerificator
+  def execute
+    pieces = Pack::Piece.where("updated_at >= ?", 12.months.ago).where(is_finalized: false).order(updated_at: :desc)
+
+    messages = []
+
+    pieces.each_with_index do |piece, index|
+      messages << "piece_id: #{piece.id}, piece_name: #{piece.name}, updated_at: #{piece.updated_at.strftime('%d-%m-%Y')}" if index <= 10
+
+      Pack::Piece.delay_for(10.seconds, queue: :high).finalize_piece(piece.id)
+    end
+
+    {
+      title: "NotFinalizedPiece - #{pieces.size} piece(s)",
+      type: "table",
+      message: messages.join('; ')
+    }
+  end 
+end
