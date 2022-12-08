716cf35f9df8e8052bb6a56e078b03ca416a9f3e
Paul <bouhpaul@gmail.com>
2022-12-08 17:53:53 +0300

    Customer view : bank documents

--- app/templates/front/retrieved_datas/assets/javascripts/events.js
+++ app/templates/front/retrieved_datas/assets/javascripts/events.js
@@ -10,7 +10,9 @@ function bind_all_events(){
   $('select#account_id').bind('change', function(e){ AppEmit('budgeaApi.user_changed', { user_id: $(this).val() }); AppEmit('retrieved_datas_reload_all') });
 
   $('.modal button.validate-filter').unbind('click');
-  $('#search-form .btn-search').bind('click', function(e){ AppEmit('retrieved_datas_filter', { type: $(this).attr('data-target') }) });
+  $('#search-form .btn-search').bind('click', function(e){ AppEmit('retrieved_datas_filter', { type: $('#search-type').val() }) });
+  $('#search-form .btn-reset').bind('click', function(e){ AppEmit('retrieved_datas_reset_filter', { type: $('#search-type').val() }) });
+
   $('.modal button.validate-filter').bind('click', function(e){ AppEmit('retrieved_datas_filter', { type: $(this).attr('data-target') }) });
 
   $('button.reset-filter').unbind('click');
@@ -19,6 +21,16 @@ function bind_all_events(){
   $('.do-showDocument').unbind('click').bind('click', function(e){
     $('#retrieved_document.modal').find('.modal-body iframe#retrieved-document').attr('src', $(this).attr('data-url'));
     $('#retrieved_document.modal').modal('show');
+  })
+
+  $('.check-filter-form-view').unbind('click').bind('click', function(e){
+    let type = $(this).attr('id').split('-')[0]
+    $('#search-type').val(type);
+
+    $('.content-search-form').hide('');
+    $('#search-' + type).show('');
+
+    $('.search-title #search-action').text($(this).data('bs-title').toLowerCase())
   })
 
 }
