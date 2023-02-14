0a416da2e29db9cd3c4f02e12355954a02af9b98
Mirana RAKOTONIRINA <mirana@idocus.com>
2023-02-14 08:34:34 +0300

    update download file

--- app/templates/front/export_preseizures/controllers/main_controller.rb
+++ app/templates/front/export_preseizures/controllers/main_controller.rb
@@ -2,9 +2,18 @@ class ExportPreseizures::MainController < FrontController
   prepend_view_path('app/templates/front/export_preseizures/views')
 
   def index
-    #@preassignment_export = PreAssignmentExport.find(params[:id])
-    #ids = @user.accounts.ids
-    @preassignment_exports = PreAssignmentExport.last 5
+    @preassignment_exports = @user.pre_assignment_exports
+    #@preassignment_exports = PreAssignmentExport.last 5
+  end
+
+
+  def download_export_preseizures
+    
+    preassignment_export_id = params["q"]
+    preassignment_export = PreAssignmentExport.find(preassignment_export_id)
+    send_data(preassignment_export.cloud_content_object.path, disposition: 'attachment')
+
+
   end
 
 end
