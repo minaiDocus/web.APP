542a5fb068d448e6191f5a556bf7b80585cebba2
Mirana RAKOTONIRINA <mirana@idocus.com>
2023-02-14 08:34:34 +0300

    update download file

--- app/templates/front/export_preseizures/routes.rb
+++ app/templates/front/export_preseizures/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   scope module: 'export_preseizures' do
 
     get 'list_export_preseizures', to: 'main#index', as: 'export_preseizures_list'
-  
+    get 'download_export_preseizures/:q', to: 'main#download_export_preseizures', as: 'download_export_preseizures'
+
   end
 
 end
