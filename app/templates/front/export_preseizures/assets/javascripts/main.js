63dd032040d10d61a9f4608ffd5306801497b4a4
Mirana RAKOTONIRINA <mirana@idocus.com>
2023-02-28 17:57:50 +0300

    preseizure export filter

---
+++ app/templates/front/export_preseizures/assets/javascripts/main.js
@@ -0,0 +1,63 @@
+jQuery(function() {
+
+  $('#document_filter').asMultiSelect({
+    "texts" : { "searchplaceholder": "Choix dossiers", "noItemsAvailable": 'Aucun dossier trouvé'},
+    "resultsContainer": '.result-sol-document',
+    "maxHeight": "500px",
+    "noneText": "Choix dossier",
+    "showSelectAll": false
+  });
+
+    $('#journal_filter').asMultiSelect({
+    "texts" : { "searchplaceholder": "Choix dossiers", "noItemsAvailable": 'Aucun dossier trouvé'},
+    "resultsContainer": '.result-sol-journal',
+    "maxHeight": "500px",
+    "noneText": "Choix dossier",
+    "showSelectAll": false
+  });
+
+    $('#period_filter').asMultiSelect({
+    "texts" : { "searchplaceholder": "Choix dossiers", "noItemsAvailable": 'Aucun dossier trouvé'},
+    "resultsContainer": '.result-sol-period',
+    "maxHeight": "500px",
+    "noneText": "Choix dossier",
+    "showSelectAll": false
+  });
+
+
+  $('#export-filter').unbind('click').bind('click',function(e) {
+    let applicationJS = new ApplicationJS();
+    let datas         = $('form#preseizure-export-filter-form').serializeObject();
+
+    let ajax_params =  {
+                          'url': '/preseizures_export',
+                          'type': 'GET',
+                          'data': datas,
+                          'dataType': 'html'
+                        }
+
+    applicationJS.sendRequest(ajax_params).then((element)=>{
+      $('#table_preseizure_export').parent().html($(element).find('#table_preseizure_export'));
+    });
+  });
+
+
+
+    $('#launch-reinit').unbind('click').bind('click',function(e) {
+    let applicationJS = new ApplicationJS();
+
+    let ajax_params =  {
+                          'url': '/preseizures_export',
+                          'type': 'GET',
+                          'dataType': 'html'
+                        }
+
+    applicationJS.sendRequest(ajax_params).then((element)=>{
+      $('#table_preseizure_export').parent().html($(element).find('#table_preseizure_export'));
+    });
+
+  });
+
+
+});
+
--- app/templates/front/export_preseizures/controllers/main_controller.rb
+++ app/templates/front/export_preseizures/controllers/main_controller.rb
@@ -2,8 +2,33 @@ class ExportPreseizures::MainController < FrontController
   prepend_view_path('app/templates/front/export_preseizures/views')
 
   def index
-    @preassignment_exports = @user.pre_assignment_exports.where('DATE_FORMAT(created_at, "%Y%m%d") >= 20230216').where('created_at >= ?', 4.month.ago)
+
+    params[:page]     = params[:page].presence || 1
+    params[:per_page] = params[:per_page].presence || 20
+
+
+    @filter_datas = { documents: [], journals: [], periods: [] }
+
+    all_preassignment_exports = @user.pre_assignment_exports.where('DATE_FORMAT(created_at, "%Y%m%d") >= 20230216').where('created_at >= ?', 4.month.ago)
+    #@preassignment_exports = @user.pre_assignment_exports
+
+    @preassignment_exports = @preassignment_exports.where( params[:document_filter].map{ |dl| "pack_name LIKE '%#{ dl.gsub('%','\%') } %'" }.join(' OR ') ) if params[:document_filter].present?
+    @preassignment_exports = @preassignment_exports.where( params[:journal_filter].map{ |jl| "pack_name LIKE '% #{jl} %'" }.join(' OR ') )                  if params[:journal_filter].present?
+    @preassignment_exports = @preassignment_exports.where( params[:period_list].map{ |pl| "pack_name LIKE '% #{pl}%'" }.join(' OR ') )                      if params[:period_list].present?
+
+    all_pack_name = @preassignment_exports.distinct.pluck(:pack_name).compact
+
+      all_pack_name.each do |pack_name|
+        @filter_datas[:documents] << pack_name.split(' ')[0]  if not @filter_datas[:documents].include?pack_name.split(' ')[0]
+        @filter_datas[:journals] << pack_name.split(' ')[1]   if not @filter_datas[:journals].include?pack_name.split(' ')[1]
+        @filter_datas[:periods] << pack_name.split(' ')[2]    if not @filter_datas[:periods].include?pack_name.split(' ')[2]
+
+      end
+
+
+    @preassignment_exports = @preassignment_exports.page(params[:page]).per(params[:per_page])
   end
+
 
   def download_export_preseizures
     preassignment_export_id = params["p"]
