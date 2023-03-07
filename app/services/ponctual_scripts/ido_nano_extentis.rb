4f19845e4bceaee64ff84138aa95864615ac460e
Mina RAKOTONIRAINY <mina@idocus.com>
2023-03-07 12:30:13 +0300

    Extentis: change customers package to ido Nano - review

--- app/services/ponctual_scripts/ido_nano_extentis.rb
+++ app/services/ponctual_scripts/ido_nano_extentis.rb
@@ -19,10 +19,17 @@ class PonctualScripts::IdoNanoExtentis < PonctualScripts::PonctualScript
       package = customer.my_package
 
       next if package.name == 'ido_nano'
+
       logger_infos "[IdoNanoExtentis] - customers code : #{customer.code.to_s} - Start"
 
-      package.bank_active             = false if ["ido_micro", "ido_micro_plus"].include?(package.name) && extents_exception.include?(customer.code)
+      if ["ido_micro", "ido_micro_plus"].include?(package.name)
+        package.bank_active = extents_exception.include?(customer.code) ? true : false 
+      end
+
       package.name                    = 'ido_nano'
+      package.upload_active           = true
+      package.scan_active             = true
+      package.preassignment           = true
       package.commitment_start_period = period
       package.commitment_end_period   = CustomUtils.period_of(12.month.after)
 
@@ -34,5 +41,23 @@ class PonctualScripts::IdoNanoExtentis < PonctualScripts::PonctualScript
     end
   end
 
-  def backup; end
+  def backup
+    organization      = Organization.find_by_code 'EXT'
+    customers         = organization.customers.active_at(Time.now)
+    period            = CustomUtils.period_of(Time.now) - 1
+
+    customers.each do |customer|
+      last_package    = customer.packages.of_period(period).first
+      current_package = customer.my_package
+
+      current_package.destroy
+
+      new_package        = last_package.dup
+      new_package.period = period + 1
+
+      new_packages.save
+
+      logger_infos "[IdoNanoExtentis] - customers code : #{customer.code.to_s} - Rollback"
+    end
+  end
 end
