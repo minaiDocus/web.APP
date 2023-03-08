eed4a5cc75f1ae114171a17c9d86635e178a3117
Mina RAKOTONIRAINY <mina@idocus.com>
2023-03-08 15:42:30 +0300

    Fix devise original query

--- app/templates/front/documents/controllers/abase_controller.rb
+++ app/templates/front/documents/controllers/abase_controller.rb
@@ -430,6 +430,8 @@ class Documents::AbaseController < FrontController #Must be loaded first that's 
 
     @s_params[:by_all]        = @s_params[:by_all].dup.reject{|k, v| k == "position_operation" } if @s_params[:by_all].present? && (@s_params[:by_all].try(:[], :position).blank? || @s_params[:by_all].try(:[], :position).split(',').size > 1)
     @s_params[:by_preseizure] = @s_params[:by_preseizure].dup.reject{|k, v| k == "amount_operation" } if @s_params[:by_preseizure].present? && @s_params[:by_preseizure].try(:[], :amount).blank?
+    @s_params[:by_preseizure] = @s_params[:by_preseizure].dup.reject{|k, v| k == "devise_operation" } if @s_params[:by_preseizure].present? && @s_params[:by_preseizure].try(:[], :devise_original).blank?
+
     @s_params = deep_compact(@s_params).with_indifferent_access
 
     @filters = {}
